568c8f525c96bea583082707280a348f9d96a3bb
karolkkk <s15195@pjwstk.edu.pl>
2019-01-20 21:21:54 +0100

    IS3 adding upgradingBuildings

--- db_control.py
+++ db_control.py
@@ -28,3 +28,54 @@ def register(data):
 def create_world(name, size = 25, capacity = 4):
         world = worlds.create_world(name, size, capacity)
         return world
+def upgrade_building(playerId, buildingId):
+        #pobranie obiektu z bazy danych po ID
+        currentBuilding = buildings.read_building(buildingId)
+        player = players.player_read(playerId)
+        upgradedBuilding = buildings.get_buildings(currentBuilding['name'],currentBuilding['lvl']+1) #jaki ma byc po upgradzie
+        #czy playera stac na budynek 
+        #woodcost
+        check={}
+        bool = False
+        if upgradedBuilding['woodCost']> player['deski']:
+                check['deski']='0'
+                bool= True
+        else:
+                check['deski']='1'
+        #capscost
+        if upgradedBuilding['capsCost'] >player['kapsle']:
+                check['kapsle']='0'
+                bool= True
+        else:
+                 check['kapsle']='1'
+        #points
+        if upgradedBuilding['pointsCost']>player['actionPoints']:
+                check['actionPoints']='0'
+                bool= True
+        else:
+                 check['actionPoints']='1'
+        #jezeli ktoregos surowca jest za malo
+        if bool==True:
+                return check, -1
+        #jezeli wszysto sie zgadza
+        player['deski']= player['deski']-upgradedBuilding['woodCost']
+        player['kapsle']= player['kapsle']-upgradedBuilding['capsCost']
+        player['pointsCost']= player['actionPoints']-upgradedBuilding['pointsCost']
+        sendToActonTable={}
+        sendToActonTable['player']=player['id']
+        sendToActonTable['status']='uncompleted'
+        sendToActonTable['building']=currentBuilding['name']
+        sendToActonTable['action']='buildingUpgrade'
+        sendToActonTable['world']=player['worldId']
+        updatedplayer= players.player_update(player,player['id'])
+        #TODO wyslac sendtoaction 
+        return updatedplayer, None
+
+
+
+
+        
+        
+              
+        
+
