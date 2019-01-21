2d42a2ec462ac472a48c8ac29f4b6015790bb3e7
s15664 <32327167+s15664@users.noreply.github.com>
2019-01-21 20:27:50 +0100

    IS10 fix to previous commit

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ def attack_square():
     player = db_control.players.player_read(content.get('playerid'))
     square = db_control.worldMap.read_square(content.get('squareid'))
     if player['world'] != square['world']:
-        return jsonify(msg = "Player is in world" + square['world'] + ", square is in " + player['world']), 408
+        return jsonify(msg = "Player is in world" + str(square['world']) + ", square is in " + str(player['world'])), 408
 
     return jsonify(msg = db_control.attack(player,square,bullets))
     
